01034b7143d240fe426f6a153ed39e94cd0064ae
Vincent Zheng <zhengvincent0825@gmail.com>
2024-09-05 16:40:33 -0400

    Added menu with settings and list view

--- mapController.js
+++ mapController.js
@@ -1,8 +1,11 @@
-var cameraListByArea = {}
+var cameraListByArea = {};
+var cameraIDsByArea = {};
+var filterList = new Set();
+var map = undefined;
 
 document.addEventListener('DOMContentLoaded', async function () {
     // Initialize Map
-    var map = L.map('map', {zoomControl: false}).setView([40.730610, -73.935242], 11);
+    map = L.map('map', {zoomControl: false}).setView([40.730610, -73.935242], 11);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://carto.com/legal">Carto</a>'
     }).addTo(map);
@@ -21,7 +24,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                 var marker = L.marker([camera.latitude, camera.longitude], {
                     icon: L.icon({
                         iconUrl: 'assets/cctv_icon.png',
-                        iconSize: [38, 38],
+                        iconSize: [38, 45],
                         iconAnchor: [18.5, 45],
                         popupAnchor: [0, -46]
                     })
@@ -48,16 +51,20 @@ document.addEventListener('DOMContentLoaded', async function () {
                 // Add to Camera list
                 if (cameraListByArea[camera.area] == undefined) {
                     cameraListByArea[camera.area] = [camera];
+                    cameraIDsByArea[camera.area] = [marker];
                 } else {
                     cameraListByArea[camera.area].push(camera);
+                    cameraIDsByArea[camera.area].push(marker);
                 }
             });
         });
 });
 
 function getCameraList(area, list) {
+    if (filterList.has(area)) return '';
+
     var elem = `<div class="areaList">
-                    <h2>${area}</h2>`;
+                    <h3>${area}</h3>`;
 
     for (var i = 0; i < list.length; i++) {
         if (i != 0) {
@@ -81,6 +88,7 @@ var cameraIntervalId = undefined;
 var currCameraId = undefined;
 var isMenuActive = false;
 var currentMenuSelection = undefined;
+var mapClustering = true;
 
 function setCameraURL() {
     cameraImage.src = `https://webcams.nyctmc.org/api/cameras/${currCameraId}/image?cacheAvoidance=${Math.floor(Math.random() * 100000)}`;
@@ -106,28 +114,32 @@ function closeCamera() {
     currCameraId = undefined;
 }
 
-window.addEventListener("click", (event) => {
-    if (event.target == cameraBox) {
-        closeCamera();
-    }
-})
-
 function toggleOpenMenu() {
     if (currentMenuSelection == undefined) {
         controlMenu.style.display = "block";
         currentMenuSelection = "List";
-        document.getElementById(`${currentMenuSelection}menuSelectButton`).disabled = true;
         
+        // Open opacity background
+        menuContainingBox.style.height = '100vh';
+        menuContainingBox.style.width = '100vw';
+
+        // Display Menu Content
+        document.getElementById(`${currentMenuSelection}menuSelectButton`).disabled = true;
         setMenuContent('List');
 
     } else {
         controlMenu.style.display = "none";
         currentMenuSelection = undefined;
 
+        // Close Menu
         setMenuContent(null);
         for (var i = 0; i < controlMenuButtons.children.length; i++) {
             controlMenuButtons.children[i].disabled = false;
         }
+
+        // Hide opacity background
+        menuContainingBox.style.height = '0';
+        menuContainingBox.style.width = '0';
     }
 }
 
@@ -143,6 +155,21 @@ function switchMenuPages(event) {
     currentMenuSelection = newPage;
 }
 
+function filterToggle(element) {
+    toggleName = element.id.split('Checkbox')[0];
+    if (element.checked) {
+        filterList.delete(toggleName);
+        cameraIDsByArea[toggleName].forEach((marker) => {
+            map.addLayer(marker);
+        })
+    } else {
+        filterList.add(toggleName);
+        cameraIDsByArea[toggleName].forEach((marker) => {
+            map.removeLayer(marker);
+        })
+    }
+}
+
 function setMenuContent(menuPage) {
     var innerContent = '';
     switch (menuPage) {
@@ -151,10 +178,51 @@ function setMenuContent(menuPage) {
                 innerContent += getCameraList(area, cameraListByArea[area]);
             }
             break;
-    
-        default:
+        
+        case "Settings":
+            var getFilterList= () => {
+                var filters = '';
+
+                Object.keys(cameraListByArea).forEach(area => {
+                    filters += `
+                        <div>
+                            <input type="checkbox"
+                                   id="${area}Checkbox"
+                                   name="${area}Checkbox"
+                                   ${filterList.has(area) ? null : 'checked'}
+                                   onchange={filterToggle(this)}
+                                />
+                            <label for="${area}Checkbox">${area}</label>
+                        </div>`;
+                });
+
+                return filters;
+            }
+
+            innerContent = `<div id="mapOptions" >
+                    <div class="mapSetting">
+                        Map Clustering
+                        <label class="switch">
+                            <input type="checkbox" ${mapClustering ? 'checked' : null}>
+                            <span class="slider round"></span>
+                        </label>
+                    </div>
+                </div>
+                <h3>Filters</h3>
+                <fieldset>
+                    <legend>New York City</legend>
+                    ${getFilterList()}
+                </fieldset>`
             break;
     }
 
     menuContentBox.innerHTML = innerContent;
 }
+
+window.addEventListener("click", (event) => {
+    if (event.target == cameraBox) {
+        closeCamera();
+    } else if (event.target == menuContainingBox) {
+        toggleOpenMenu();
+    }
+})
